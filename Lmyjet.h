0d541ea8329f567052090e15ff1de5d530a331a5
sabazb <saba.zarbaf1376@gmail.com>
2017-02-06 17:43:17 +0330

    Update Lmyjet.h

--- Lmyjet.h
+++ Lmyjet.h
@@ -5,16 +5,18 @@ class LogicJet{
 private:
  
     int PosX;
+    int Fuel;
     int PosY;
     int Width;
     int Height;
 public:
     LogicJet();
-   
+    int getFuel();
     int getPosX();
     int getPosY();
     void setPosX(int x);
     void setPosY(int y);
+    void setFuel(int F);
     int getWidth();
     int getHeight();
 };
