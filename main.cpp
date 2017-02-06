b165862c3d9c045912119f4127d6f03afac5867b
amoloudi <mohammadaminmoloudi12369@gmail.com>
2017-02-06 17:22:58 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 // main
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+    
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+    
+    // create a view to visualize the scene
+    QGraphicsView * view = new QGraphicsView(scene);
+    
+    // show the view
+    view->show();
+    view->setFixedSize(1280,720);
+    scene->setSceneRect(0,0,1280,720);
+    
     return a.exec();
 }
