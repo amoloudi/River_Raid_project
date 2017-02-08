6686e0b3f963839286dea5a0144e4208b9c88649
amoloudi <mohammadaminmoloudi12369@gmail.com>
2017-02-08 14:49:18 +0330

    Update Gbullet.h

--- Gbullet.h
+++ Gbullet.h
@@ -3,11 +3,13 @@
 
 #include <QGraphicsPixmapItem>
 #include <QObject>
+#include <QMediaPlayer>
 #include "lbullet.h"
 
 class Bullet: public QObject,public QGraphicsPixmapItem{
     Q_OBJECT
 private:
+    QMediaPlayer * explosion;
     LogicBullet * NewBullet;
 public:
     Bullet(int x, int y);
