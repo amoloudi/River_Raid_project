8071b6653d1e57d9e2e302dadffc6da349e7d321
sabazb <saba.zarbaf1376@gmail.com>
2017-02-08 12:18:07 +0330

    Create Gmyjet.h

---
+++ Gmyjet.h
@@ -0,0 +1,24 @@
+#ifndef MYJET_H
+#define MYJET_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include "globals.h"
+
+class MyJet:public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+public:
+    MyJet(QGraphicsScene * scene);
+    void keyPressEvent(QKeyEvent * event);
+public slots:
+    void spawnHeli();
+    void spawnShip();
+    void spawnJet();
+    void spawnBalloon();
+    void spawnFuelDepot();
+    void DecreaseFuel();
+};
+
+#endif // MYJET_H
