068503f313bc73a8e3247d8f1d0f80fa00c5cc94
sabazb <saba.zarbaf1376@gmail.com>
2017-02-09 12:55:35 +0330

    Update Gmyjet.cpp

--- Gmyjet.cpp
+++ Gmyjet.cpp
@@ -6,11 +6,14 @@
 #include "genemyballoon.h"
 #include "gfueldepot.h"
 #include <QKeyEvent>
+#include "fuel.h"
 #include "gameover.h"
 #include "globals.h"
 
 MyJet::MyJet(QGraphicsScene *scene)
 {
+    bulletsound = new QMediaPlayer();
+    bulletsound->setMedia(QUrl("qrc:/sounds/bullet_sound.mp3"));
 
     GJet = new LogicJet();
     setPixmap(QPixmap(":/images/jet.png"));
@@ -21,6 +24,16 @@ MyJet::MyJet(QGraphicsScene *scene)
     // make rect focusable
     this->setFlag(QGraphicsItem::ItemIsFocusable);
     this->setFocus();
+
+    // adding fuel
+    fuel = new Fuel();
+    fuel->setPos(fuel->x(),fuel->y()+650);
+    scene->addItem(fuel);
+
+    // adding score
+    score = new Score();
+    score->setPos(score->x(),score->y()+600);
+    scene->addItem(score);
 }
 
 void MyJet::keyPressEvent(QKeyEvent *event){
@@ -28,12 +41,32 @@ void MyJet::keyPressEvent(QKeyEvent *event){
     if (event->key() == Qt::Key_Left){
         if (GJet->getPosX() > 0)
         {
+            if(GJet->getPosX() - 10 < 295)
+            {
+                // print GAME OVER
+                GameOver* gameover = new GameOver();
+                gameover->setPos(425,300);
+                scene()->addItem(gameover);
+                delete this;
+                return;
+            }
             setPos(x()-10,y());
             GJet->setPosX(GJet->getPosX()-10);
         }
     }
     //Move Right - CheckRight Border
     else if (event->key() == Qt::Key_Right){
+        if (GJet->getPosX() + GJet->getWidth() < 1280)
+        {
+            if(GJet->getPosX() + GJet->getWidth() + 10 > 985)
+            {
+                // print GAME OVER
+                GameOver* gameover = new GameOver();
+                gameover->setPos(425,300);
+                scene()->addItem(gameover);
+                delete this;
+                return;
+            }
             setPos(x()+10,y());
             GJet->setPosX(GJet->getPosX()+10);
         }
@@ -44,6 +77,14 @@ void MyJet::keyPressEvent(QKeyEvent *event){
             Bullet * bullet = new Bullet(GJet->getPosX(), GJet->getPosY());
             bullet->setPos(x()+10,y()-10);
             scene()->addItem(bullet);
+
+            // play bulletsound
+            if (bulletsound->state() == QMediaPlayer::PlayingState){
+                bulletsound->setPosition(0);
+            }
+            else if (bulletsound->state() == QMediaPlayer::StoppedState){
+                bulletsound->play();
+            }
     }
 }
 
@@ -77,4 +118,20 @@ void MyJet::spawnFuelDepot()
     // create a Fuel Depot
     FuelDepot * enemy = new FuelDepot();
     scene()->addItem(enemy);
+}
+
+void MyJet::DecreaseFuel()
+{
+    if(GJet->getFuel() > 0)
+        GJet->setFuel(GJet->getFuel()-1);
+    fuel->update();
+    if(GJet->getFuel() == 0)
+    {
+        // print GAME OVER
+        GameOver* gameover = new GameOver();
+        gameover->setPos(425,300);
+        scene()->addItem(gameover);
+        delete this;
+        return;
+    }
 }
